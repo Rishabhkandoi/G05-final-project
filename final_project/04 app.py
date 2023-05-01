95ea407c50d8082c3ac3c1bed07a4bcb3fa6ac02
Rishabhkandoi <Rishabhkandoi>
2023-05-01 00:02:14 +0000

    With corrected graph of future predictions

--- final_project/04 app.py
+++ final_project/04 app.py
@@ -3,6 +3,13 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC # Agenda for this notebook
+# MAGIC - To understand about the assigned station like it's location, capacity, etc.
+# MAGIC - To report the model performance(residuals) and forecasts.
+
+# COMMAND ----------
+
 #libraries to be imported
 import plotly.express as px
 import pandas as pd
@@ -103,6 +110,24 @@ display(temp_df_data.filter(col("last_reported") <= currenthour).sort(desc("last
 
 # COMMAND ----------
 
+# for next HOURS_TO_FORECAST 
+model_info = spark.read.format("delta").load(MODEL_INFO)
+model_info = model_info.orderBy(desc("ds"))
+filtered_model_info = model_info.filter(model_info.tag == "Production")
+new_model_info = filtered_model_info.limit(HOURS_TO_FORECAST)
+model_info = new_model_info.drop("y", "tag", "residual", "mae")
+from pyspark.sql.functions import to_timestamp, date_format
+# converting timestamp
+model_info = model_info.withColumn("ds", to_timestamp("ds", "yyyy-MM-dd'T'HH:mm:ss.SSSZ")) \
+                       .withColumn("ds", date_format("ds", "yyyy-MM-dd HH:mm:ss"))
+model_info = model_info.withColumn("yhat", round("yhat", 0))
+from pyspark.sql.functions import lit
+model_info = model_info.withColumn("avail", lit(None))
+# saving only ds, yhat and avail
+from pyspark.sql.functions import asc
+
+last_date = model_info.select("ds").orderBy(asc("ds")).limit(1).collect()[0]["ds"]
+
 #reading bike stream data with hour_window and availability columns
 real_time_inventory_data = spark.read.format("delta").load(REAL_TIME_INVENTORY_INFO_DELTA_DIR)
 real_time_inventory_data = real_time_inventory_data.orderBy("hour_window", ascending=False)
@@ -117,71 +142,56 @@ w = Window.orderBy("hour_window")
 real_time_inventory_data = real_time_inventory_data.withColumn("diff", col("avail") - lag(col("avail"), 1).over(w))
 real_time_inventory_data = real_time_inventory_data.withColumn("diff", coalesce(col("diff"), col("avail")))
 real_time_inventory_data = real_time_inventory_data.orderBy("hour_window", ascending=False)
-from pyspark.sql.functions import monotonically_increasing_id
-real_time_inventory_data = real_time_inventory_data.withColumn('index', monotonically_increasing_id())
-# removing first n values from diff column and making it null, we will impute our forecast
-from pyspark.sql.functions import when, col
-diff_new_df = real_time_inventory_data.withColumn('diff_new', when(col('index').between(0, 7), None).otherwise(col('diff')))
-display(diff_new_df)
 
-# COMMAND ----------
 
-#loading the model information from Gold table
-model_output_df = spark.read.format("delta").load(MODEL_INFO)
-display(model_output_df)
+# real time streaming data till currentdate (variable which has current time)
+trial_new_df = real_time_inventory_data
+trial_new_df = trial_new_df.filter(trial_new_df.hour_window < last_date)
+new_trial = trial_new_df.select("hour_window", "avail", "diff").orderBy(desc("hour_window"))
+#renaming columns for same schema
+new_trial = new_trial.withColumnRenamed("diff", "yhat")
+new_trial = new_trial.withColumnRenamed("hour_window", "ds")
 
-model_output = model_output_df.toPandas()
-model_output["yhat"] = model_output["yhat"].round()
-model_output["capacity"] = 61
+# reorder columns in new_trial
+new_trial = new_trial.select("ds", "avail", "yhat")
+# reorder columns in model_info
+model_info = model_info.select("ds", "avail", "yhat")
+# merging datas
+merged_df = model_info.union(new_trial)
+merged_df = merged_df.orderBy(desc("ds"))
 
-staging_forecast_df = model_output[model_output.tag == "Staging"]
-prod_forecast_df = model_output[model_output.tag == "Production"]
-forecast_df = prod_forecast_df.iloc[-hours_to_forecast:,:]
-# creating a new dataframe with just yhat values and index column
-forecast_temp = forecast_df[["yhat"]]
-forecast_temp["index"] = list(range(0, hours_to_forecast))
-forecast_temp = spark.createDataFrame(forecast_temp)
 
-# Join the dataframes based on the index column
-merged_df = forecast_temp.join(diff_new_df, on='index', how='outer')
-
-from pyspark.sql.functions import when
-
-#imputing yhat values that we predicted into y
-imputed_df = merged_df.withColumn(
-    "diff_new", 
-    when(merged_df["diff_new"].isNull(), merged_df["yhat"]).otherwise(merged_df["diff_new"])
-)
-
-# cumulative addition of  diff_new values to find new_available (our prediction of how many bikes will be available)
 from pyspark.sql.functions import col, sum as spark_sum
 from pyspark.sql.window import Window
-imputed_df = imputed_df.orderBy("hour_window", ascending=True)
-window = Window.orderBy("hour_window")
-imputed_df = imputed_df.withColumn("new_available", spark_sum(col("diff_new")).over(window))
-imputed_df = imputed_df.orderBy("hour_window", ascending=False)
-
-pd_plot = imputed_df.toPandas()
-pd_plot = pd_plot.iloc[:hours_to_forecast,:]
-pd_plot["capacity"] = STATION_CAPACITY #defined in includes file 
+# calculating new_available using window
+merged_df = merged_df.orderBy("ds", ascending=True)
+window = Window.orderBy("ds")
+merged_df = merged_df.withColumn("new_available", spark_sum(col("yhat")).over(window))
+merged_df = merged_df.orderBy("ds", ascending=False)
+display(merged_df)
 
 # COMMAND ----------
 
 #plotting the computed forecasts
 import plotly.express as px
 import plotly.graph_objects as go
+
+pd_plot = merged_df.toPandas()
+pd_plot = pd_plot.iloc[:hours_to_forecast,:]
+pd_plot["capacity"] = STATION_CAPACITY #defined in includes file 
+
 fig = go.Figure()
 pd_plot["zero_stock"] = 0
-fig.add_trace(go.Scatter(x=pd_plot.hour_window, y=pd_plot["new_available"], name='Forecasted available bikes',mode = 'lines+markers',
+fig.add_trace(go.Scatter(x=pd_plot.ds, y=pd_plot["new_available"], name='Forecasted available bikes',mode = 'lines+markers',
                          line = dict(color='blue', width=3, dash='solid')))
-fig.add_trace(go.Scatter(x=pd_plot.hour_window[:4], y=pd_plot["new_available"][:4], mode = 'markers',name='Forecast for next 4 hours',
+fig.add_trace(go.Scatter(x=pd_plot.ds[-4:], y=pd_plot["new_available"][-4:], mode = 'markers',name='Forecast for next 4 hours',
                          marker_symbol = 'triangle-up',
                          marker_size = 15,
                          marker_color="green"))
-fig.add_trace(go.Scatter(x=pd_plot.hour_window, y=pd_plot["capacity"], name='Station Capacity (Overstock beyond this)',
+fig.add_trace(go.Scatter(x=pd_plot.ds, y=pd_plot["capacity"], name='Station Capacity (Overstock beyond this)',
                          line = dict(color='red', width=3, dash='dot')))
-fig.add_trace(go.Scatter(x=pd_plot.hour_window, y=pd_plot["zero_stock"], name='Stock Out (Understock below this)',
-                         line = dict(color='red', width=3, dash='dot')))
+fig.add_trace(go.Scatter(x=pd_plot.ds, y=pd_plot["zero_stock"], name='Stock Out (Understock below this)',
+                         line = dict(color='orange', width=3, dash='dot')))
 # Edit the layout
 fig.update_layout(title='Forecasted number of available bikes',
                    xaxis_title='Forecasted Timeline',
@@ -191,15 +201,8 @@ fig.show()
 
 # COMMAND ----------
 
-# import gmaps
-# gmaps.configure(api_key="AIzaSyCfs22cuSZakjMdM34lNgWYG5Y2eHyiAHQ")
-# #40.734814,-73.992085
-# new_york_coordinates = (40.730610, -73.975242)
-# marker_loc = [(STATION_LAT,STATION_LON)]
-# fig = gmaps.figure(center=new_york_coordinates, zoom_level=14)
-# markers = gmaps.marker_layer(marker_loc)
-# fig.add_layer(markers)
-# fig
+# MAGIC %md
+# MAGIC ■ Monitor the performance of your staging and production models using an appropriate residual plot that illustrates the error in your forecasts.
 
 # COMMAND ----------
 
